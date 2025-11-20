fc6149b61394dad01d918727ee881421d84f30d8
krjohnn <ks91ko@gmail.com>
2025-11-21 01:59:33 +0200

    delete old scripts folder

--- MD2/scripts/1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "hello world"
-
-rm -f deleteme.txt
-
-mkdir -p new_dir
-
-date > new_dir/date.txt
--- MD2/scripts/2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-A=$(cat a)
-B=$(cat b)
-result=$((A * B))
-
-echo "$result" > c
